39a34d8e876475e9048a0df3a016efbf3de2075b
Gurkaran Singh <gurkaran@grevity.in>
2020-11-18 01:35:41 +0530

    done

---
+++ client/src/components/Layout/TopNav/index.js
@@ -0,0 +1,44 @@
+import React, { PropTypes } from 'react';
+import { AppBar, Paper, IconButton, FlatButton, FontIcon } from 'material-ui';
+import { NavigationClose } from 'material-ui/svg-icons';
+import { Responsive, Badge } from 'components';
+import Logo from '../Logo';
+import styles from './styles';
+
+function TopNav({ menuOpen, onLeftIconButtonTouchTap, onRightIconButtonTouchTap }) {
+  const title = (
+    <div>
+      <Responsive size="desktop">
+      </Responsive>
+      <Responsive size="mobile" />
+    </div>
+  )
+  const iconElementRight = (
+    <div style={styles.notificationsIconWrapper}>
+      
+    </div>
+  );
+  return (
+    <Paper zDepth={2}>
+      <AppBar
+        title={title}
+        onLeftIconButtonTouchTap={onLeftIconButtonTouchTap}
+        iconElementRight={iconElementRight}
+      />
+    </Paper>
+  );
+}
+
+TopNav.propTypes = {
+  menuOpen: PropTypes.bool,
+  onLeftIconButtonTouchTap: PropTypes.func,
+  onRightIconButtonTouchTap: PropTypes.func,
+};
+
+TopNav.defaultProps = {
+    menuOpen: false,
+    onLeftIconButtonTouchTap: () => {},
+    onRightIconButtonTouchTap: () => {},
+};
+
+export default TopNav;
